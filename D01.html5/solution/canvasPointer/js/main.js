fbfb89b31af1b8536b077df6b24a0019ead8a384
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2023-12-14 09:45:57 +0200

    add canvasPointer 2 and 3

--- D01.html5/solution/canvasPointer/js/main.js
+++ D01.html5/solution/canvasPointer/js/main.js
@@ -1,10 +1,34 @@
-const box = document.createElement("div");
+// const box = document.createElement("div");
 
-document.addEventListener("click", function (e) {
-  const x = e.pageX;
-  const y = e.pageY;
-  box.classList.add("box");
-  box.style.left = `${x - 25}px`;
-  box.style.top = `${y - 25}px`;
-  document.body.appendChild(box);
-});
+// document.addEventListener("click", function (e) {
+//   const x = e.pageX;
+//   const y = e.pageY;
+//   //const box = document.createElement("div"); //canus 2
+//   box.classList.add("box");
+//   box.style.left = `${x - 25}px`;
+//   box.style.top = `${y - 25}px`;
+//   document.body.appendChild(box);
+// });
+
+/*-------------------------------------------------------*/
+
+// let timerId;
+
+// document.addEventListener("mousedown", function (e) {
+//   let x = e.clientX;
+//   let y = e.clientY;
+//   timerId = setInterval(() => {
+//     document.addEventListener("mousemove", function (e) {
+//       x = e.clientX;
+//       y = e.clientY;
+//     });
+//     const box = document.createElement("div");
+//     box.classList.add("hold");
+//     box.style.left = `${x - 25}px`;
+//     box.style.top = `${y - 25}px`;
+//     document.body.appendChild(box);
+//   }, 50);
+// });
+// document.addEventListener("mouseup", function (e) {
+//   clearInterval(timerId);
+// });
